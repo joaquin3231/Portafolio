fb46586059055f8accf61448975747b18d2c9986
Pintos Joaquin <joale7546@gmail.com>
2024-08-16 11:31:33 -0300

    iniciar Repositorio

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const navEnlaces = document.querySelectorAll(".link-ubicacion");
+const enlace = document.querySelector(".link-ubicacion");
+
+const yo = document.querySelector("#yo");
+const sobreMi = document.querySelector("#sobreMi");
+const proyectos = document.querySelector("#proyectos");
+const educacion = document.querySelector("#educacion");
+const habilidades = document.querySelector("#habilidades");
+const contactos = document.querySelector("#contactos");
+
+enlace, addEventListener("mousedown", (e) => {
+    const { parentElement, id, classList } = e.target
+    if (parentElement.classList.contains("nav-izq")) {
+        for (let i = 0; i < navEnlaces.length; i++) {
+            if (navEnlaces[i].classList.contains("activado") && navEnlaces[i].id !== id) {
+                navEnlaces[i].classList.remove("activado")
+            }
+        }
+        if (classList.contains("activado")) {
+            classList.remove("activado")
+        } else {
+            classList.add("activado")
+        }
+        // console.log(id);
+    }
+})
